f2f2c70f15ad62d63e667b9bbd50de97cfacdfe2
6h2i43p6k6fwyypo327vbpifnfcy57repa5pbolwibxhao5z4nfq <6h2i43p6k6fwyypo327vbpifnfcy57repa5pbolwibxhao5z4nfq>
2017-03-21 10:49:00 -0300

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+    public static void main(String arg[]){
+    	System.out.println("Hello world!");
+    	System.out.println("Hello world sonar runner!");
+    }
+
+}
